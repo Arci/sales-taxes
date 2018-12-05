2d526e74495714ff3f525e3a899cd726194707c9
Fabio Arcidiacono <fa.arcidiacono@gmail.com>
2018-12-05 08:12:30 +0100

    main class that reads cli arguments and uses SalesTaxes

---
+++ src/main/java/it/arcidiacono/salestaxes/Main.java
@@ -0,0 +1,132 @@
+package it.arcidiacono.salestaxes;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import it.arcidiacono.salestaxes.model.Receipt;
+import it.arcidiacono.salestaxes.model.ShoppingBasket;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+
+	private static final String BASKET_OPTION = "b";
+
+	private static final String CATEGORIES_OPTION = "c";
+
+	private static final String OUTPUT_OPTION = "o";
+
+	public static void main(String[] args) {
+		Options options = defineOptions();
+
+		CommandLineParser parser = new DefaultParser();
+		CommandLine commandLine = null;
+		try {
+			commandLine = parser.parse(options, args);
+		} catch (ParseException e) {
+			log.error("unable to parse arguments: {}", e.getMessage());
+			help(options);
+			System.exit(0);
+		}
+
+		InputStream basket = null;
+		InputStream categories = null;
+		try {
+			basket = getOptionStream(commandLine, BASKET_OPTION);
+			categories = getOptionStream(commandLine, CATEGORIES_OPTION);
+		} catch (IOException e) {
+			log.error("cannot read input files", e);
+			System.exit(0);
+		}
+
+		Path outputLocation = getOptionPath(commandLine, OUTPUT_OPTION);
+		log.debug("output arg: {}", outputLocation);
+		if (Files.notExists(outputLocation)) {
+			log.warn("output file does not exists, will be created", outputLocation);
+			try {
+				outputLocation = Files.createFile(outputLocation);
+			} catch (IOException e) {
+				log.error("cannot create output file: {}", outputLocation, e);
+				System.exit(0);
+			}
+		}
+
+		try {
+			String receipt = buildReceipt(basket, categories);
+			writeRecipt(outputLocation, receipt);
+		} catch (IOException e) {
+			log.error("an error occurred calculating the receipt: {}", e.getMessage(), e);
+			System.exit(0);
+		}
+	}
+
+	private static Options defineOptions() {
+		Option basket = Option.builder(BASKET_OPTION)
+				.required()
+				.hasArg()
+				.longOpt("basket")
+				.desc("the shopping basket file path")
+				.build();
+
+		Option categories = Option.builder(CATEGORIES_OPTION)
+				.required()
+				.hasArg()
+				.longOpt("categories")
+				.desc("the categories file path")
+				.build();
+
+		Option output = Option.builder(OUTPUT_OPTION)
+				.required()
+				.hasArg()
+				.longOpt("output")
+				.desc("the file to which output the receipt")
+				.build();
+
+		final Options options = new Options();
+		options.addOption(basket);
+		options.addOption(categories);
+		options.addOption(output);
+		return options;
+	}
+
+	private static void help(Options options) {
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp("SalesTaxes", options);
+	}
+
+	private static Path getOptionPath(CommandLine commandLine, String option) {
+		return Paths.get(commandLine.getOptionValue(option));
+	}
+
+	private static InputStream getOptionStream(CommandLine commandLine, String option) throws FileNotFoundException {
+		Path path = getOptionPath(commandLine, option);
+		return new FileInputStream(path.toFile());
+	}
+
+	private static String buildReceipt(InputStream basket, InputStream categories) throws IOException {
+		SalesTaxes salesTaxes = new SalesTaxes();
+		salesTaxes.read(categories);
+		ShoppingBasket shoppingBasket = salesTaxes.parse(basket);
+		Receipt receipt = salesTaxes.buildReceipt(shoppingBasket);
+		return salesTaxes.format(receipt);
+	}
+
+	private static void writeRecipt(Path outputLocation, String receipt) throws IOException {
+		Files.write(outputLocation, receipt.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+	}
+
+}
